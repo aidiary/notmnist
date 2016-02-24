8e7714965b02e0d3fbb0c0e56fa4b515490325bb
aidiary <f2forest@gmail.com>
2016-02-24 19:23:00 +0900

    Add a merge_datasets()

--- preprocess.py
+++ preprocess.py
@@ -15,6 +15,11 @@ url = 'http://yaroslavvb.com/upload/notMNIST/'
 num_classes = 10  # A to J
 image_size = 28
 pixel_depth = 255.0
+
+train_size = 200000
+valid_size = 10000
+test_size = 10000
+
 np.random.seed(133)
 
 
@@ -149,6 +154,50 @@ def verify_datasets(datasets):
         print("%s: %d" % (pickle_file, len(letter_set)))
 
 
+def make_arrays(nb_rows, img_size):
+    if nb_rows:
+        dataset = np.ndarray((nb_rows, img_size, img_size), dtype=np.float32)
+        labels = np.ndarray(nb_rows, dtype=np.int32)
+    else:
+        dataset, labels = None, None
+    return dataset, labels
+
+def merge_datasets(pickle_files, train_size, valid_size=0):
+    num_classes = len(pickle_files)
+    valid_dataset, valid_labels = make_arrays(valid_size, image_size)
+    train_dataset, train_labels = make_arrays(train_size, image_size)
+    vsize_per_class = valid_size // num_classes
+    tsize_per_class = train_size // num_classes
+
+    start_v, start_t = 0, 0
+    end_v, end_t = vsize_per_class, tsize_per_class
+    end_l = vsize_per_class + tsize_per_class
+    for label, pickle_file in enumerate(pickle_files):
+        try:
+            with open(pickle_file, 'rb') as f:
+                letter_set = pickle.load(f)
+                # shuffle the letters to have
+                # random validation and training set
+                np.random.shuffle(letter_set)
+                if valid_dataset is not None:
+                    valid_letter = letter_set[:vsize_per_class, :, :]
+                    valid_dataset[start_v:end_v, :, :] = valid_letter
+                    valid_labels[start_v:end_v] = label
+                    start_v += vsize_per_class
+                    end_v += vsize_per_class
+
+                train_letter = letter_set[vsize_per_class:end_l, :, :]
+                train_dataset[start_t:end_t, :, :] = train_letter
+                train_labels[start_t:end_t] = label
+                start_t += tsize_per_class
+                end_t += tsize_per_class
+        except Exception as e:
+            print('Unable to process data from', pickle_file, ':', e)
+            raise
+
+        return valid_dataset, valid_labels, train_dataset, train_labels
+
+
 if __name__ == '__main__':
     train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
     test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
@@ -174,3 +223,14 @@ if __name__ == '__main__':
 
     verify_datasets(train_datasets)
     verify_datasets(test_datasets)
+
+    valid_dataset, valid_labels, train_dataset, train_labels = \
+        merge_datasets(train_datasets, train_size, valid_size)
+
+    _, _, test_dataset, test_labels = \
+        merge_datasets(test_datasets, test_size)
+
+    print('Training:', train_dataset.shape, train_labels.shape)
+    print('Validation:', valid_dataset.shape, valid_labels.shape)
+    print('Testing:', test_dataset.shape, test_labels.shape)
+
