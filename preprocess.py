16561e7727e586d74d882338ace1e316ecea8fcf
aidiary <f2forest@gmail.com>
2016-02-24 19:54:24 +0900

    Dump datasets to pickle file

--- preprocess.py
+++ preprocess.py
@@ -251,7 +251,27 @@ if __name__ == '__main__':
     valid_dataset, valid_labels = randomize(valid_dataset, valid_labels)
     test_dataset, test_labels = randomize(test_dataset, test_labels)
 
-    verify_class_balance(train_labels)
-    verify_class_balance(valid_labels)
-    verify_class_balance(test_labels)
+    # verify_class_balance(train_labels)
+    # verify_class_balance(valid_labels)
+    # verify_class_balance(test_labels)
 
+    # Save the data for later reuse
+    pickle_file = 'notMNIST.pickle'
+    try:
+        f = open(pickle_file, 'wb')
+        save = {
+            'train_dataset': train_dataset,
+            'train_labels': train_labels,
+            'valid_dataset': valid_dataset,
+            'valid_labels': valid_labels,
+            'test_dataset': test_dataset,
+            'test_labels': test_labels,
+        }
+        pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+        f.close()
+    except Exception as e:
+        print('Unable to save data to', pickle_file, ':', e)
+        raise
+
+    statinfo = os.stat(pickle_file)
+    print('Compressed pickle size:', statinfo.st_size)
