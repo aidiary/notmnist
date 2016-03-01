54d51a8d468a7749aaf0e34b163fc9ce23a4f22f
aidiary <f2forest@gmail.com>
2016-03-01 22:11:48 +0900

    Add loading pickle codes

---
+++ fullyconnected.py
@@ -0,0 +1,20 @@
+import numpy as np
+from six.moves import cPickle as pickle
+import tensorflow as tf
+
+if __name__ == "__main__":
+    pickle_file = 'notMNIST.pickle'
+
+    with open(pickle_file, 'rb') as f:
+        save = pickle.load(f)
+        train_dataset = save['train_dataset']
+        train_labels = save['train_labels']
+        valid_dataset = save['valid_dataset']
+        valid_labels = save['valid_labels']
+        test_dataset = save['test_dataset']
+        test_labels = save['test_labels']
+        del save
+        print('Training set', train_dataset.shape, train_labels.shape)
+        print('Validation set', valid_dataset.shape, valid_labels.shape)
+        print('Test set', test_dataset.shape, test_labels.shape)
+
