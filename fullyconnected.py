b9e04e3185211ab3a108865ba9650dae0ff69742
aidiary <f2forest@gmail.com>
2016-03-05 21:17:20 +0900

    Add training codes

--- fullyconnected.py
+++ fullyconnected.py
@@ -2,6 +2,23 @@ import numpy as np
 from six.moves import cPickle as pickle
 import tensorflow as tf
 
+image_size = 28
+num_labels = 10
+
+train_subset = 10000
+num_steps = 801
+
+def reformat(dataset, labels):
+    dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+    # Map 0 to [1,0,0,...], 1 to [0,1,0,...]
+    labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+def accuracy(predictions, labels):
+    return (100.0 *
+            np.sum(np.argmax(predictions, axis=1) == np.argmax(labels, axis=1)) /
+            predictions.shape[0])
+
 if __name__ == "__main__":
     pickle_file = 'notMNIST.pickle'
 
@@ -17,4 +34,53 @@ if __name__ == "__main__":
         print('Training set', train_dataset.shape, train_labels.shape)
         print('Validation set', valid_dataset.shape, valid_labels.shape)
         print('Test set', test_dataset.shape, test_labels.shape)
+
+    # Reformat datasets and labels
+    train_dataset, train_labels = reformat(train_dataset, train_labels)
+    valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+    test_dataset, test_labels = reformat(test_dataset, test_labels)
+    print('Training set', train_dataset.shape, train_labels.shape)
+    print('Validation set', valid_dataset.shape, valid_labels.shape)
+    print('Test set', test_dataset.shape, test_labels.shape)
+
+    graph = tf.Graph()
+    with graph.as_default():
+        # Input dataset
+        tf_train_dataset = tf.constant(train_dataset[:train_subset, :])
+        tf_train_labels = tf.constant(train_labels[:train_subset])
+        tf_valid_dataset = tf.constant(valid_dataset)
+        tf_test_dataset = tf.constant(test_dataset)
+
+        # Variables
+        weights = tf.Variable(
+          tf.truncated_normal([image_size * image_size, num_labels]))
+        biases = tf.Variable(tf.zeros([num_labels]))
+
+        # Training computation
+        # (10000, 784) x (784, 10) = (10000, 10)
+        logits = tf.matmul(tf_train_dataset, weights) + biases
+        loss = tf.reduce_mean(
+          tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+
+        # Optimizer
+        optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+
+        # Predictions for the training, validation, and test data
+        train_prediction = tf.nn.softmax(logits)
+        valid_prediction = tf.nn.softmax(tf.matmul(tf_valid_dataset, weights) + biases)
+        test_prediction = tf.nn.softmax(tf.matmul(tf_test_dataset, weights) + biases)
+
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        print('Initiallized')
+
+        for step in range(num_steps):
+            _, l, predictions = session.run([optimizer, loss, train_prediction])
+            if (step % 100 == 0):
+                print('Loss at step %d: %f' % (step, l))
+                print('\tTraining accuracy: %.1f%%' % accuracy(
+                      predictions, train_labels[:train_subset, :]))
+                print('\tValidation accuracy: %.1f%%' % accuracy(
+                      valid_prediction.eval(), valid_labels))
+        print('Test accuracy: %.1f%%' % accuracy(test_prediction.eval(), test_labels))
 
