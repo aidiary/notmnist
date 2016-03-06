25a9e5677d3ae68ea58a0dd28fd3b848e535f3e3
aidiary <f2forest@gmail.com>
2016-03-06 20:03:37 +0900

    Add tensorflow sample codes

---
+++ tf/fully_connected_feed.py
@@ -0,0 +1,10 @@
+"""Trains and Evaluates the MNIST network using a feed dictionary."""
+
+import os.path
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
